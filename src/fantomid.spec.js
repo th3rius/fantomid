50a826b1a3d6e7b0d662f369334c5ec64129a9fa
Gabriel de Oliveira <gabrielhk3@gmail.com>
2025-11-28 18:15:21 -0300

    remove console.log from test

--- src/fantomid.spec.js
+++ src/fantomid.spec.js
@@ -14,7 +14,6 @@ test("fantomid", () => {
     randomMock.mock.mockImplementationOnce(() =>
       Buffer.from([0xa2, 0xd2, 0xff]),
     );
-    console.log(Buffer.from([0xa2, 0xd2, 0xff, 0x04, 0xc5]));
     mock.timers.enable({ now: new Date("2025-11-24T19:13:45.994Z") });
     const id = fantomid();
     assert.equal(id, 8209108814378532);
